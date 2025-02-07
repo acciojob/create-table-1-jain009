d9d323c753793e8cac22701da61f6a8b7a2ff1d2
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-08 00:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
+  document.addEventListner("DOMContentded", () => {
+	 documnet.getElementById("insert_row").addEventListner("onclick", () => {
+		 const table = document.getElementById("sampleTable");
+		 const newRow = table.insertRow(0);
+		  const columnCount = table.rows[1].cells.length;
+    for (let i = 0; i < columnCount; i++) {
+      const newCell = newRow.insertCell(i);
+      newCell.textContent = `New Cell ${i + 1}`;
+    } 
+	 })
+  })
   
 }
